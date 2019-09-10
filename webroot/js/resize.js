a57804f4205bdf7687c120fe2f402c089dd95399
Hunter Gregory <hlg16@duke.edu>
2019-09-10 00:01:21 -0400

    put resize and load in same call and added xxxs font size

--- webroot/js/resize.js
+++ webroot/js/resize.js
@@ -1,19 +1,14 @@
-function handleResize() {
+$(window).on('load resize', function() {
     const xs = 993;//768;
     const xxs = 600;//768;
+    const xxxs = 370;
     const width = $(window).width();
     var fontSize = 27;
-    if (width <= xxs)
+    if (width <= xxxs)
+        fontSize = 10;
+    else if (width <= xxs)
         fontSize = 12;
     else if (width <= xs)
         fontSize = 18;
     $("#myhtml, body, div.main-content").css("font-size", fontSize + "px");
-}
-
-$(window).on('load', function() {
-    handleResize();
-});
-
-$(window).on('resize', function() {
-    handleResize();
 });
